6928d71f2c848a990f1195a11ad3c08eff0c32e6
Lanai Conceição <lanai.conceicao@mercadolivre.com>
2023-04-06 09:22:13 -0300

    feat(index.html, webpack.config.js, src/index.js): module 1 - setup webpack

---
+++ modulo_1/react-webpack/webpack.config.js
@@ -0,0 +1,12 @@
+// Configuration to read a file and generate a bundle
+'use strict'
+
+const path = require('path')
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'index'),
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  }
+}

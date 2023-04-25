3784050e740898b8c920289ba078d44be42e1685
Lanai Conceição <lanai.conceicao@mercadolivre.com>
2023-04-25 09:54:29 -0300

    feat(.babelrc, webpack.config, server.js, package.json, package-lock.json): module 1 - config react hot loader

--- modulo_1/react-webpack/webpack.config.js
+++ modulo_1/react-webpack/webpack.config.js
@@ -2,18 +2,31 @@
 'use strict'
 
 const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
   // says to webpack to create a source map of the file
   // source map: map of the main file -> make debug easier
   devtool: 'source-map',
 
-  entry: path.join(__dirname, 'src', 'index'),
+  entry: [
+    // ========================
+    // React hot loader -> Do not need to refresh a page to update when we change a component in React
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    // ========================
+    path.join(__dirname, 'src', 'index')
+  ],
 
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
 
   module: {
     loaders: [{

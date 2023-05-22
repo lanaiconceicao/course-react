d1ef8301c6baeeec3af0e74a7e88529aa4d2ea72
Lanai Conceição <lanai.conceicao@mercadolivre.com>
2023-05-22 10:09:33 -0300

    feact(title) - add default props

--- modulo_1/s3_m1_p2/src/title.js
+++ modulo_1/s3_m1_p2/src/title.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
-const Title = ({ name }) => {
+const Title = ({ name, lastname }) => {
   return (
-    <h1>{`Olá ${name}`}</h1>
+    <h1>{`Olá ${name} ${lastname}`}</h1>
   )
 }
 
 export default Title
+
+Title.defaultProps = {
+  name: 'Desconhecido',
+  lastname: 'Sobrenome desconhecido'
+}
